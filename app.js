45ed3d542823d35340badebde518bffa4a011e43
adriantheodor <aatheodor@gmail.com>
2022-11-23 08:41:21 -0500

    a8 REST fa22 9

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ import TuitsController from "./controllers/tuits/tuits-controller.js";
 const app = express();
 
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 TuitsController(app);
 HelloController(app);
 UserController(app);
 app.listen(process.env.PORT || 4000);
-console.log("done")
+console.log("done!!!")
